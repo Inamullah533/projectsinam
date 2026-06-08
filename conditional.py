ebd9917055b6d15941ed98ca8e0269eed4035d25
Inamullah533 <inam.ullah.waada@gmail.com>
2026-06-08 04:29:05 -0700

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+if num1 > num2:
+    print("The first number is greater.")
+elif num2 > num1:
+    print("The second number is greater.")
+elif num1 == num2:
+    print("Both numbers are equal.")
+else:
+    print("Invalid Input")
