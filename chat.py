82e21a1f23dd130448d61d5ef53cdf7e4a42dbb7
Inamullah533 <inam.ullah.waada@gmail.com>
2026-06-08 04:29:36 -0700

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,14 @@
+
+a=str(input("Hello, Husnain! How are you?")).lower().strip() 
+if a=="i am fine":
+    print("Good to hear that! husnain. ")
+elif a=="i am not fine":
+    print("I am sorry to hear that! husnain. ")
+else:
+    print(" I am sure you will be fine soon husnain.")
+b=str(input("How can I help You? I am here for your help.")).lower().strip()
+if b=="i want to know about you":
+    print("I am a chatbot created by Husnain. I am here to assist you with any questions or concerns you may have. Feel free to ask me anything!")
+else:
+    print("I am sorry, I can only provide information about myself. Please ask me about myself if you want to know more.")
+
